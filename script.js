2b1371b3686dec729b06dd496a14597c34fc0aff
Daniel Grosman <danielgrosman9@gmail.com>
2025-11-15 00:27:30 -0500

    AM and PM

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 function runClock() {
     const clock = document.getElementById("clock")
     const current = new Date();
-    const hours = current.getHours().toString().padStart(2, 0);
+    let hours = current.getHours();
+    const meridian = hours >= 12 ? "PM" : "AM";
+    hours = hours % 12 || 12;
+    hours = hours.toString().padStart(2, 0)
     const minutes = current.getMinutes().toString().padStart(2, 0);
     const seconds = current.getSeconds().toString().padStart(2, 0);
-    const display = `${hours}:${minutes}:${seconds}`;
+    const display = `${hours}:${minutes}:${seconds} ${meridian}`;
 
     clock.textContent = display;
 }
